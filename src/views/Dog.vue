edd188dbd6f71d70592d8f564cf43f8903662303
Kingsley <kingsleyumujeyan@gmail.com>
2021-01-28 22:56:30 +0100

    ft--updated and refactored codebase

--- src/views/Dog.vue
+++ src/views/Dog.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-        <h1>Dogs for Adoption</h1>
-        <b-table striped hover :items="dogs">
-          <template #cell(name) ='data'>
-              <router-link :to="`/pets/dogs/${data.index}`">
-                {{ data.value }}
-              </router-link>
-            </template>
-        </b-table>
+         <PetTable
+        species='Dogs'
+        :pets='dogs'
+       />
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import PetTable from '../components/PetTable.vue';
 
 export default {
+  components: {
+    PetTable,
+  },
   data() {
     return {};
   },
